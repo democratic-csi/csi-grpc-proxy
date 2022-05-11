023e87aea080b92a278c8bc4a62ecf4e8b4019b3
Travis Glenn Hansen <travisghansen@yahoo.com>
2022-05-10 18:30:02 -0600

    remove dependency on docker with the buildah commands
    Signed-off-by: Travis Glenn Hansen <travisghansen@yahoo.com>

--- .github/bin/docker-release-windows.sh
+++ .github/bin/docker-release-windows.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login         -u "$DOCKER_USERNAME" --password-stdin
-echo "$GHCR_PASSWORD"   | docker login ghcr.io -u "$GHCR_USERNAME"   --password-stdin
+set -e
 
 export DOCKER_ORG="democraticcsi"
 export DOCKER_PROJECT="csi-grpc-proxy"
-export DOCKER_REPO="${DOCKER_ORG}/${DOCKER_PROJECT}"
+export DOCKER_REPO="docker.io/${DOCKER_ORG}/${DOCKER_PROJECT}"
 
 export GHCR_REPO="ghcr.io/${GITHUB_REPOSITORY}"
 
@@ -17,7 +16,7 @@ if [[ -n "${IMAGE_TAG}" ]]; then
   buildah manifest create "${MANIFEST_NAME}"
   
   # all all the existing linux data to the manifest
-  buildah manifest add "${MANIFEST_NAME}" --all "docker.io/${DOCKER_REPO}:${IMAGE_TAG}"
+  buildah manifest add "${MANIFEST_NAME}" --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" --all "${DOCKER_REPO}:${IMAGE_TAG}"
   buildah manifest inspect "${MANIFEST_NAME}"
   
   # import pre-built images
@@ -30,8 +29,8 @@ if [[ -n "${IMAGE_TAG}" ]]; then
   buildah manifest inspect "${MANIFEST_NAME}"
 
   # push manifest
-  buildah manifest push --all "${MANIFEST_NAME}" docker://docker.io/${DOCKER_REPO}:${IMAGE_TAG}
-  buildah manifest push --all "${MANIFEST_NAME}" docker://${GHCR_REPO}:${IMAGE_TAG}
+  buildah manifest push --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" --all "${MANIFEST_NAME}" docker://${DOCKER_REPO}:${IMAGE_TAG}
+  buildah manifest push --creds "${GHCR_USERNAME}:${GHCR_PASSWORD}"     --all "${MANIFEST_NAME}" docker://${GHCR_REPO}:${IMAGE_TAG}
 
   # cleanup
   buildah manifest rm "${MANIFEST_NAME}" || true
